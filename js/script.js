782ea71b109cdffdd507edc1dd299d1794e24eae
Gabriel Russell <gabrielrussell@Gabriels-MacBook-Air.local>
2020-02-03 10:57:22 -0600

    adds latest updates

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
 /*----- constants -----*/
+const KEY = {
+	'1': 'X',
+	'-1': 'O',
+	'null': ''
+};
 
+const COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 /*----- app's state (variables) -----*/
-let 
+let turn, gameboard, winner;
 
 /*----- cached element references -----*/
-
+const messageEl = document.getElementById('message');
+const squareEls = document.querySelectorAll('.square');
 
 /*----- event listeners -----*/
-
+document.getElementById('game-board').addEventListener('click', handleClick);
 
 /*----- functions -----*/
-init();
+// init();
 
-function init() {
+// function init() {
+	
+// }
 
+function handleClick(e) {
+    console.log(e.target.dataset.index)
 }
